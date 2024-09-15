ebc2abc7c8ce2aace72dc1c1386f0ced36170291
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-09-15 15:13:02 -0300

    Layout header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+export default function Header() {
+    return (
+        <header className='de4-header'>
+            <nav className="de4-container">
+                <h1>DS Filter</h1>
+                <div de4-nav-right>6 produto(s)</div>
+            </nav>
+        </header>
+    )
+}
